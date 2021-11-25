175d76983bdf47e9aaf023583ca8a1956b529550
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2021-11-25 22:30:19 +0200

    load esp32 sketch

---
+++ esp32sketch/esp32sketch.ino
@@ -0,0 +1,91 @@
+#include <WebServer.h>
+#include <WiFi.h>
+#include <esp32cam.h>
+
+
+const char *WIFI_SSID = "example-ssid";
+const char *WIFI_PASS = "example-pass";
+
+WebServer server(80);
+
+static auto cam_res = esp32cam::Resolution::find(800, 600);
+
+
+void handle_mjpeg(){
+	Serial.println("Stream begin");
+
+	WiFiClient client = server.client();
+	int result = esp32cam::Camera.streamMjpeg(client);
+
+	if(result <= 0){
+		Serial.println("Stream error");
+		return;
+	}
+
+	Serial.println("Stream end");
+}
+
+
+void handle_jpg(){
+	auto frame = esp32cam::capture();
+
+	if(frame == nullptr){
+		Serial.println("Capture error");
+		server.send(503, "", "");
+		return;
+	}
+
+	server.setContentLength(frame->size());
+	server.send(200, "image/jpeg");
+
+	WiFiClient client = server.client();
+	frame->writeTo(client);
+
+	Serial.println("Capture ok");
+}
+
+
+void setup(){
+	Serial.begin(115200);
+	Serial.println();
+
+	{
+		using namespace esp32cam;
+
+		Config config;
+
+		config.setPins(pins::AiThinker);
+		config.setResolution(cam_res);
+		config.setBufferCount(2);
+		config.setJpeg(80);
+
+		bool is_camera_ok = Camera.begin(config);
+
+		if(is_camera_ok) Serial.println("Camera ok");
+		else Serial.println("Camera error");
+	}
+
+	WiFi.persistent(false);
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+	while(WiFi.status() != WL_CONNECTED) delay(400);
+
+	server.on("/cam.jpg", handle_jpg);
+	server.on("/cam.mjpeg", handle_mjpeg);
+
+	server.begin();
+
+	Serial.print("http://");
+	Serial.print(WiFi.localIP());
+	Serial.println("/cam.mjpeg");
+
+	Serial.print("http://");
+	Serial.print(WiFi.localIP());
+	Serial.println("/cam.jpg");
+}
+
+
+void loop(){
+	server.handleClient();
+}
