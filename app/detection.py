7bc9db50e44dd077fe187a635c9f1051f1051921
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2022-01-04 23:17:04 +0200

    add object detection logic

---
+++ app/detection.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+
+from tflite_runtime.interpreter import Interpreter
+
+
+class ObjectDetector:
+
+	def __init__(self, tflite_model, threshold, image_res):
+		self.interpreter = Interpreter(model_path=tflite_model)
+		self.interpreter.allocate_tensors()
+
+		self.input_details = self.interpreter.get_input_details()
+		self.output_details = self.interpreter.get_output_details()
+		self.height = self.input_details[0]['shape'][1]
+		self.width = self.input_details[0]['shape'][2]
+
+		self.floating_model = (self.input_details[0]['dtype'] == np.float32)
+
+		self.input_mean = 127.5
+		self.input_std = 127.5
+
+		self.threshold = threshold
+		self.res_x, self.res_y = image_res
+
+	def get_contours(self, image):
+		resized_image = cv2.resize(image.copy(), (self.width, self.height))
+		input_data = np.expand_dims(resized_image, axis=0)
+
+		if self.floating_model:
+			input_data = (np.float32(input_data) - self.input_mean) / self.input_std
+
+		self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
+		self.interpreter.invoke()
+
+		boxes = self.interpreter.get_tensor(self.output_details[1]['index'])[0]
+		scores = self.interpreter.get_tensor(self.output_details[0]['index'])[0]
+
+		contours = []
+
+		for i in range(len(scores)):
+			if scores[i] >= self.threshold and scores[i] <= 1.0:
+				y_min = int(max(1, (boxes[i][0] * self.res_y)))
+				x_min = int(max(1, (boxes[i][1] * self.res_x)))
+				y_max = int(min(self.res_y, (boxes[i][2] * self.res_y)))
+				x_max = int(min(self.res_x, (boxes[i][3] * self.res_x)))
+
+				contours.append([x_min, y_min, x_max, y_max])
+
+		return contours
--- app/gui.py
+++ app/gui.py
@@ -2,9 +2,14 @@ import os
 import time
 from urllib.request import urlretrieve
 
-from PySide6.QtWidgets import QMainWindow, QWidget, QTabWidget, QPushButton, QVBoxLayout, QFileDialog
+import cv2
+
+from PySide6.QtWidgets import QMainWindow, QWidget, QTabWidget, QPushButton, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout, QFileDialog
 from PySide6.QtWebEngineWidgets import QWebEngineView
 from PySide6.QtCore import QUrl
+from PySide6.QtGui import QPixmap
+
+from detection import ObjectDetector
 
 
 class MainWindow(QMainWindow):
@@ -13,10 +18,11 @@ class MainWindow(QMainWindow):
 		super(MainWindow, self).__init__()
 
 		self.setWindowTitle('Shadow microscope')
-		self.resize(640, 480)
+		self.resize(800, 600)
 
 		tab_widget = QTabWidget()
 		tab_widget.addTab(StreamTab('http://192.168.0.107'), 'Live video')
+		tab_widget.addTab(ProcessingTab('neural-network/model_100k.tflite', 0.4, (800, 600)), 'Image processing')
 
 		self.setCentralWidget(tab_widget)
 
@@ -57,3 +63,54 @@ class StreamTab(QWidget):
 			os.replace('temp.jpg', save_path)
 
 		self.web_view.reload()
+
+
+class ProcessingTab(QWidget):
+
+	def __init__(self, tflite_model, threshold, image_res):
+		super(ProcessingTab, self).__init__()
+
+		self.detector = ObjectDetector(tflite_model, threshold, image_res)
+
+		self.path_input = QLineEdit(placeholderText='Path to image...')
+
+		path_button = QPushButton('...')
+		path_button.clicked.connect(self.path_button_click)
+
+		detect_button = QPushButton('Start processing')
+		detect_button.clicked.connect(self.detect_button_click)
+
+		h_layout = QHBoxLayout()
+		h_layout.addWidget(self.path_input)
+		h_layout.addWidget(path_button)
+		h_layout.addWidget(detect_button)
+
+		h_widget = QWidget()
+		h_widget.setLayout(h_layout)
+
+		self.image_label = QLabel() 
+		self.update_image('extra-files/blank.png')
+
+		v_layout = QVBoxLayout()
+		v_layout.addWidget(h_widget)
+		v_layout.addWidget(self.image_label)
+
+		self.setLayout(v_layout)
+
+	def path_button_click(self):
+		image_path = QFileDialog.getOpenFileName(filter='*.jpg')
+		self.path_input.setText(image_path[0])
+
+	def detect_button_click(self):
+		image = cv2.imread(self.path_input.text())
+
+		for contour in self.detector.get_contours(image):
+			x_min, y_min, x_max, y_max = contour
+			image = cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 255, 0), 1)
+			cv2.imwrite('extra-files/temp.jpg', image)
+
+		self.update_image('extra-files/temp.jpg')
+
+	def update_image(self, image_path):
+		image_pixmap = QPixmap(image_path)
+		self.image_label.setPixmap(image_pixmap)
